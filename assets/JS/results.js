a4b4775d9996173d6a750283bf1592b356b41bf7
Victoria Benoit <vrose834@gmail.com>
2023-03-13 13:31:58 -0300

    Got the other parts displaying & added styling

--- assets/JS/results.js
+++ assets/JS/results.js
@@ -1,11 +1,13 @@
 let searchBtn= document.getElementById('searchInside');
 let backBtn = document.getElementById('back');
 let resultsCont = document.getElementById('resultsCont');
-let format = document.getElementById('format').value;
-let searchingFor = document.getElementById('searchingFor').value;
+
 
 
 function search(){
+    let format = document.getElementById('format').value;
+    let searchingFor = document.getElementById('searchingFor').value;
+
     let libraryURL = 'https://www.loc.gov/'+ format + '?q=' + searchingFor + '&fo=json';
     //let libraryURL = "https://www.loc.gov/collections/world-war-i-sheet-music/?fo=json";
     fetch(libraryURL)
@@ -19,10 +21,10 @@ function search(){
 }
 function displaySearch(data){
     resultsCont.textContent = " ";
-    if(data.featured_items.length === 0){
+    if(data.results.length === 0){
         resultsCont.innerHTML = 'No results found';
     }
-    for(let i = 0; i < data.featured_items.length; i++){
+    for(let i = 0; i < data.results.length; i++){
         let resultDiv = document.createElement('div');
         resultDiv.classList.add('resultDiv');
 
@@ -30,19 +32,33 @@ function displaySearch(data){
         resultBody.classList.add('divBody');
 
         let titleEl = document.createElement('h3');
-        titleEl.textContent = JSON.stringify(data.featured_items[i].title);
+        titleEl.classList.add('titleEl');
+        titleEl.textContent = JSON.stringify(data.results[i].title).slice(1, -1);
+        
+        
+        let dateEl = document.createElement('h3');
+        dateEl.classList.add('dateEl');
+        dateEl.textContent = JSON.stringify(data.results[i].date).slice(1, -1);
 
-        let imageEl = document.createElement('img');
-        imageEl.src = JSON.stringify(data.featured_items[i].image);
+        let subjectEl = document.createElement('h3');
+        subjectEl.classList.add('subjectEl');
+        subjectEl.textContent = JSON.stringify(data.results[i].partof).slice(1, -1);
 
         let descEl = document.createElement('h4');
+        descEl.classList.add('descEl');
+        descEl.textContent = JSON.stringify(data.results[i].description).slice(1, -1);
+        
+        let readMore = document.createElement('button');
+        readMore.textContent = 'Read  More';
+        readMore.classList.add('readMoreBtn');
+        readMore.src = JSON.stringify(data.results[i].url).slice(1, -1);
 
-        if(data.featured_items[i].description){
-            descEl.textContent = JSON.stringify(data.featured_items[i].description);
-        } else {
-            descEl.textContent = 'No description';
-        }
-        resultBody.appendChild(titleEl, descEl, imageEl);
+
+        resultBody.appendChild(titleEl);
+        resultBody.appendChild(dateEl);
+        resultBody.appendChild(subjectEl);
+        resultBody.appendChild(descEl);
+        resultBody.appendChild(readMore);
         resultDiv.appendChild(resultBody);
         resultsCont.appendChild(resultDiv);
     }
